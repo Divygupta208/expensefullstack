49279482bf104b9dd511729a06ef09476672a8d6
Divy Gupta <divygupta208@gmail.com>
2024-09-02 11:46:17 +0530

    route

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -5,5 +5,6 @@ const expenseController = require("../controllers/expense");
 
 router.post("/addexpense", expenseController.postAddExpense);
 router.get("/getexpense", expenseController.getExpenses);
+router.delete("/delete/:id", expenseController.deleteExpense);
 
 module.exports = router;
