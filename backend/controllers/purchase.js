2d4c1bef1c84e87f8c11acfc0dc8e63463b8442c
Divy Gupta <divygupta208@gmail.com>
2024-09-05 17:08:35 +0530

    purchase controller for razorpay

---
+++ backend/controllers/purchase.js
@@ -0,0 +1,70 @@
+const Order = require("../models/order");
+const User = require("../models/user");
+const Razorpay = require("razorpay");
+
+exports.purchasePremiumMembership = async (req, res) => {
+  try {
+    const razorpayInstance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+
+    const amount = 5000;
+    const user = req.user;
+
+    const order = await razorpayInstance.orders.create({
+      amount: amount,
+      currency: "INR",
+      receipt: `receipt_order_${user.id}`,
+    });
+
+    console.log(order);
+
+    const newOrder = await Order.create({
+      userId: user.id,
+      orderId: order.id,
+      status: "PENDING",
+    });
+
+    res.json({
+      order,
+      key_id: process.env.RAZORPAY_KEY_ID,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create Razorpay order" });
+  }
+};
+
+exports.updateTransactionStatus = async (req, res) => {
+  try {
+    const { order_id, payment_id } = req.body;
+    const order = await Order.findOne({ where: { orderId: order_id } });
+
+    if (!order) {
+      return res.status(404).json({ error: "Order not found" });
+    }
+
+    console.log(payment_id);
+
+    order.paymentId = payment_id;
+    order.status = "SUCCESSFUL";
+    await order.save();
+
+    const user = await User.findByPk(order.userId);
+
+    if (user) {
+      user.isPremium = true;
+      await user.save();
+    }
+
+    res.json({
+      success: true,
+      message: "Payment successful and order updated",
+      isPremium: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to update transaction status" });
+  }
+};
