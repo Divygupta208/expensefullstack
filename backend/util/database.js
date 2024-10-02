66e86ec2dd65824eaa5b4fc0291b1e70be1e35bc
Divy Gupta <divygupta208@gmail.com>
2024-10-02 15:17:23 +0530

    dist changes

--- backend/util/database.js
+++ backend/util/database.js
@@ -3,8 +3,15 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("expensetracker", "divygupta", "divy132000", {
   dialect: "mysql",
   host: "database-1.cbiwoy48oi75.ap-south-1.rds.amazonaws.com",
-
   timezone: "+05:30",
 });
 
 module.exports = sequelize;
+
+// const sequelize = new Sequelize("expense", "root", "Dg@132000", {
+//   dialect: "mysql",
+//   host: "localhost",
+//   timezone: "+05:30",
+// });
+
+// module.exports = sequelize;
